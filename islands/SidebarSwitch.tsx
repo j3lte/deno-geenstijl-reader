07044339fcd25b5f82c20ce052d397fdcc809d33
j3lte <jwlagendijk@gmail.com>
2022-07-05 00:17:26 +0200

    Update switch

--- islands/SidebarSwitch.tsx
+++ islands/SidebarSwitch.tsx
@@ -52,13 +52,14 @@ export default function SidebarSwitch() {
       <path d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z" />
     </svg>
   );
+  const icon = hideSideBarCookie === "no" ? eyeEnabled : eyeDisabled;
 
   return (
     <div
       class={tw`absolute bottom-2 left-5 opacity-60 cursor-pointer`}
       onClick={onClick}
     >
-      {hideSideBarCookie === "no" ? eyeEnabled : eyeDisabled}
+      {icon}
     </div>
   );
 }
