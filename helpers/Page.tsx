fd8e515720deea4b675382ede88de50d3c2b9e60
j3lte <jwlagendijk@gmail.com>
2022-07-04 20:37:54 +0200

    Add stuff to the head

--- helpers/Page.tsx
+++ helpers/Page.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 /** @jsxFrag Fragment */
 import { ComponentChildren, Fragment, h } from "preact";
+import { Head } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 import Header from "@/helpers/Header.tsx";
 import Footer from "@/helpers/Footer.tsx";
@@ -14,6 +15,23 @@ export function Page({
 }) {
   return (
     <>
+      <Head>
+        <title>Mini GS Reader</title>
+        <meta name="description" content="Mini GS Reader" />
+        <meta property="og:title" content="Mini GS Reader" />
+        <meta property="og:description" content="Mini GS Reader" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/images/icons-256.png"
+        />
+        <meta name="theme-color" content="#fc32a9" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <div
         class={tw`mx-auto max-w-screen-lg flex flex-col h-screen subpixel-antialiased`}
         style={{ height: `calc(var(--vh, 1vh) * 100)` }}
--- static/client_fixes.js
+++ static/client_fixes.js
@@ -9,28 +9,10 @@ calcSize();
 window.onresize = calcSize;
 
 function manifestLoader() {
-  const link = document.createElement('link');
-  link.rel = "manifest";
-  document.head.appendChild(link);
-  link.href = "/manifest.json";
-
-  const favicon = document.createElement('link');
-  favicon.rel = 'shortcut icon';
-  favicon.type = 'image/x-icon';
-  document.head.appendChild(favicon);
-  favicon.href = "/images/icons-256.png";
-
-  const themeColor = document.createElement('meta');
-  themeColor.name = 'theme-color';
-  themeColor.content = '#fc32a9';
-  document.head.appendChild(themeColor);
-
   const viewPort = document.querySelector('meta[name="viewport"]');
   if (viewPort) {
     viewPort.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no')
   }
-
-  console.log(viewPort)
 }
 
 manifestLoader();
