b48c3eaae238f0923683acf3521def515b0eb4f1
j3lte <jwlagendijk@gmail.com>
2022-07-06 11:38:37 +0200

    Error pages

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,19 +2,23 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/api/article/[slug].ts";
-import * as $1 from "./routes/arch/[slug].tsx";
-import * as $2 from "./routes/art/[slug].tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_500.tsx";
+import * as $2 from "./routes/api/article/[slug].ts";
+import * as $3 from "./routes/arch/[slug].tsx";
+import * as $4 from "./routes/art/[slug].tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/ImageDisplay.tsx";
 import * as $$1 from "./islands/SidebarSwitch.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/article/[slug].ts": $0,
-    "./routes/arch/[slug].tsx": $1,
-    "./routes/art/[slug].tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/_404.tsx": $0,
+    "./routes/_500.tsx": $1,
+    "./routes/api/article/[slug].ts": $2,
+    "./routes/arch/[slug].tsx": $3,
+    "./routes/art/[slug].tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/ImageDisplay.tsx": $$0,
